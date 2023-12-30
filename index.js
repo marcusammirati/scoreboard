05bf6d855be59eb44b1b5c679415472c8fbfc2dc
marcusammirati <153789084+marcusammirati@users.noreply.github.com>
2023-12-30 16:44:09 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+
+let homeEl = document.getElementById("home-el")
+let countHome = 0
+
+
+function oneHome() {
+    countHome += 1
+    homeEl.innerText = countHome
+}
+
+function twoHome() {
+    countHome += 2
+    homeEl.innerText = countHome
+    
+    
+}
+
+function threeHome() {
+    
+    countHome += 3
+    homeEl.innerText = countHome
+    
+}
+
+
+let awayEl = document.getElementById("away-el")
+let countAway = 0
+
+
+function oneAway() {
+    countAway += 1
+    awayEl.innerText = countAway
+    
+    
+}
+
+
+function twoAway() {
+    
+    countAway += 2
+    awayEl.innerText = countAway
+    
+}
+
+
+function threeAway() {
+    
+    countAway += 3
+    awayEl.innerText = countAway
+    
+}
